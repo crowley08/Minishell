5128fdb1f58eda7e9900bad2ee5e5aeec808f3f0
Saotra Fahasoavana Andrianome <saandria@c1r8s4.42antananarivo.mg>
2024-09-16 15:11:28 +0300

    token tsy vita

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: saandria < saandria@student.42antananar    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 21:16:12 by saandria          #+#    #+#             */
-/*   Updated: 2024/09/16 10:36:37 by saandria         ###   ########.fr       */
+/*   Updated: 2024/09/16 14:56:34 by saandria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,26 @@
 # include <sys/wait.h>
 # include "../libft/libft.h" 
 # include <term.h>
+
+typedef struct	s_token	t_token;
+typedef enum	e_tokentype	t_tokentype;
+
+enum	e_tokentype
+{
+	TOK_WORD,
+    TOK_PIPE,
+	TOK_REDIRIN,
+	TOK_REDIROUT,
+	TOK_APPEND,
+	TOK_HEREDOC,
+};
+
+struct	s_token
+{
+	t_tokentype	type;
+	char        *value;
+	t_token        *next;
+};
 
 //sig_handler
 void	handle_sigint(int sig);
--- src/main.c
+++ src/main.c
@@ -6,13 +6,64 @@
 /*   By: saandria < saandria@student.42antananar    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 15:44:01 by saandria          #+#    #+#             */
-/*   Updated: 2024/09/16 11:34:10 by saandria         ###   ########.fr       */
+/*   Updated: 2024/09/16 15:08:15 by saandria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	main(void)
+t_token *create_token(t_tokentype type, char *value, int *i)
+{
+	t_token    *new_token;
+
+    new_token = (t_token *)malloc(sizeof(t_token));
+    if (!new_token)
+        return (NULL);
+    new_token->type = type;
+    new_token->value = ft_strdup(value);
+    new_token->next = NULL;
+    return (new_token);
+}
+
+t_token	*tokenizer(char *s)
+{
+	t_token	*head;
+	t_token	*current;
+	t_token	*new_token;
+	t_token	*token;
+	int i;
+
+	i = 0;
+	head = NULL;
+	current = NULL;
+	while (s[i])
+	{
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+        {
+			i++;
+			continue ;
+        }
+        else if (s[i] == '|')
+		{
+			new_token = create_token(TOK_PIPE, "|", &i);
+		}
+		if (head == NULL)
+		{
+			head = new_token;
+			current = head;
+		}
+		else if (current == NULL)
+		{
+			current = new_token;
+            head = current;
+		}
+        else
+            i++;
+	}
+	return (token)
+}
+
+int	main(int ac, char **av, char **env)
 {
 	char	*s;
 
