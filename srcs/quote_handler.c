14732d2f8cd2c4390722a6c0d121be54df1b0661
herifanantenana <herifanantenana17@gmail.com>
2024-09-19 14:33:12 +0300

    quote_handler

--- srcs/quote_handler.c
+++ srcs/quote_handler.c
@@ -5,46 +5,47 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: arakotom <arakotom@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/18 12:10:14 by arakotom          #+#    #+#             */
-/*   Updated: 2024/09/18 16:34:11 by arakotom         ###   ########.fr       */
+/*   Created: 2024/09/19 13:57:36 by arakotom          #+#    #+#             */
+/*   Updated: 2024/09/19 14:04:54 by arakotom         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/minishell.h"
+#include "../inc/minishell.h"
 
-void	update_quote(char c, t_quote_state *d_q, t_quote_state *s_q)
+void	quote_update_state(char c, t_quote_state *d_q, t_quote_state *s_q)
 {
-	if ((c == '"' || c == '\'') && (*d_q == closed) && (*s_q == closed))
+	if (*d_q == CLOSED && *s_q == CLOSED)
 	{
 		if (c == '"')
-			*d_q = opened;
+			*d_q = OPENED;
 		else if (c == '\'')
-			*s_q = opened;
+			*s_q = OPENED;
 	}
-	else if ((c == '"' || c == '\'') && (*d_q == opened || *s_q == opened))
+	else if (*d_q == OPENED || *s_q == OPENED)
 	{
 		if (c == '"')
-			*d_q = closed;
+			*d_q = CLOSED;
 		else if (c == '\'')
-			*s_q = closed;
+			*s_q = CLOSED;
 	}
 }
 
-t_bool	is_quote_opened(char *input)
+t_bool	quote_opened(char *prompt)
 {
 	t_quote_state	d_q;
 	t_quote_state	s_q;
 	int				i;
 
 	i = 0;
-	d_q = closed;
-	s_q = closed;
-	while (input[i])
+	d_q = CLOSED;
+	s_q = CLOSED;
+	while (prompt[i])
 	{
-		update_quote(input[i], &d_q, &s_q);
+		if (ms_is_quote(prompt[i]))
+			quote_update_state(prompt[i], &d_q, &s_q);
 		i++;
 	}
-	if (d_q == opened || s_q == opened)
-		return (true);
-	return (false);
+	if (d_q == OPENED || s_q == OPENED)
+		return (TRUE);
+	return (FALSE);
 }
---
+++ srcs/quote_utils.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   quote_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arakotom <arakotom@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/19 13:58:10 by arakotom          #+#    #+#             */
+/*   Updated: 2024/09/19 14:27:34 by arakotom         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/minishell.h"
+
+t_bool	ms_is_quote(char c)
+{
+	return (c == '"' || c == '\'');
+}
+
+t_bool	ms_is_var_char(char c)
+{
+	return ((c >= 65 && c <= 90) || (c >= 97 && c <= 122) || c == 95);
+}
+
+void	quote_state_init(t_quote_state *d_q, t_quote_state *s_q)
+{
+	*d_q = CLOSED;
+	*s_q = CLOSED;
+}
