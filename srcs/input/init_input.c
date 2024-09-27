6a3fbec6b5291f8b9704498b72de56687f0fd109
Andriantseheno Herifanantenana Rakotomalala <arakotom@c1r7s6.42antananarivo.mg>
2024-09-27 15:51:10 +0300

    adding add_history

--- srcs/input/init_input.c
+++ srcs/input/init_input.c
@@ -6,7 +6,7 @@
 /*   By: arakotom <arakotom@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 23:07:00 by arakotom          #+#    #+#             */
-/*   Updated: 2024/09/27 15:39:48 by arakotom         ###   ########.fr       */
+/*   Updated: 2024/09/27 15:50:30 by arakotom         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ char *get_input(void)
 		free(input);
 		input = readline("minishell > ");
 	}
+	add_history(input);
 	return (input);
 }
