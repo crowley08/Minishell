cd2eee96720ac7e4dfa021f21e1af1857d85caa7
herifanantenana <herifanantenana17@gmail.com>
2024-09-29 23:42:44 +0300

    norminette

--- includes/envp.h
+++ includes/envp.h
@@ -6,21 +6,21 @@
 /*   By: arakotom <arakotom@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 19:44:00 by arakotom          #+#    #+#             */
-/*   Updated: 2024/09/28 13:09:34 by arakotom         ###   ########.fr       */
+/*   Updated: 2024/09/29 23:38:05 by arakotom         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ENVP_H
-#define ENVP_H
+# define ENVP_H
 
 // init_envp
-t_env *create_env(char *str);
-void add_env_list(t_env **list, t_env *env);
-t_bool dup_envp(t_data *data, char **envp);
+t_env	*create_env(char *str);
+void	add_env_list(t_env **list, t_env *env);
+t_bool	dup_envp(t_data *data, char **envp);
 
 // envp_utils
-t_env *new_env(void);
-void free_env(t_env *env);
-void free_env_list(t_env **list);
+t_env	*new_env(void);
+void	free_env(t_env *env);
+void	free_env_list(t_env **list);
 
 #endif
--- includes/types.h
+++ includes/types.h
@@ -6,34 +6,34 @@
 /*   By: arakotom <arakotom@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 00:00:47 by arakotom          #+#    #+#             */
-/*   Updated: 2024/09/28 13:53:15 by arakotom         ###   ########.fr       */
+/*   Updated: 2024/09/29 23:38:12 by arakotom         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TYPES_H
-#define TYPES_H
+# define TYPES_H
 
-typedef enum e_bool t_bool;
-typedef enum e_quote_stt t_quote_stt;
-typedef enum e_error_stt t_error_stt;
-typedef struct s_env t_env;
-typedef struct s_data t_data;
-typedef struct s_quote_dt t_quote_dt;
-typedef struct s_heredoc t_heredoc;
+typedef enum e_bool			t_bool;
+typedef enum e_quote_stt	t_quote_stt;
+typedef enum e_error_stt	t_error_stt;
+typedef struct s_env		t_env;
+typedef struct s_data		t_data;
+typedef struct s_quote_dt	t_quote_dt;
+typedef struct s_heredoc	t_heredoc;
 
-enum e_bool
+enum						e_bool
 {
 	FALSE = 0,
 	TRUE = 1
 };
 
-enum e_quote_stt
+enum						e_quote_stt
 {
 	CLOSED = 0,
 	OPENED = 1,
 };
 
-enum e_error_stt
+enum						e_error_stt
 {
 	INIT = -1,
 	NOTHING = 0,
@@ -43,32 +43,32 @@ enum e_error_stt
 	STX_REDIR = 4,
 };
 
-struct s_env
+struct						s_env
 {
-	char *name;
-	char *value;
-	t_env *next;
+	char					*name;
+	char					*value;
+	t_env					*next;
 };
 
-struct s_data
+struct						s_data
 {
-	t_env *envp;
-	char *input;
-	t_error_stt curr_error;
-	char **prompts;
+	t_env					*envp;
+	char					*input;
+	t_error_stt				curr_error;
+	char					**prompts;
 };
 
-struct s_quote_dt
+struct						s_quote_dt
 {
-	t_quote_stt d_q;
-	t_quote_stt s_q;
+	t_quote_stt				d_q;
+	t_quote_stt				s_q;
 };
-struct s_heredoc
+struct						s_heredoc
 {
-	char *eof;
-	char *filename;
-	t_bool expend_var;
-	t_heredoc *next;
+	char					*eof;
+	char					*filename;
+	t_bool					expend_var;
+	t_heredoc				*next;
 };
 
 #endif
