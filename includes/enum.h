a90d649e38b8478945230f14880abe63b9a5ebf4
herifanantenana <herifanantenana17@gmail.com>
2024-10-13 22:28:11 +0300

    create structure and enum for prompt

--- includes/enum.h
+++ includes/enum.h
@@ -6,30 +6,32 @@
 /*   By: arakotom <arakotom@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 12:46:49 by arakotom          #+#    #+#             */
-/*   Updated: 2024/10/13 12:56:57 by arakotom         ###   ########.fr       */
+/*   Updated: 2024/10/13 22:26:50 by arakotom         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ENUM_H
 # define ENUM_H
 
-typedef enum e_bool			t_bool;
-typedef enum e_quote_stt	t_quote_stt;
-typedef enum e_error_stt	t_error_stt;
+typedef enum e_bool				t_bool;
+typedef enum e_quote_stt		t_quote_stt;
+typedef enum e_error_stt		t_error_stt;
+typedef enum e_redir_out_type	t_redir_out_type;
+typedef enum e_token_type		t_token_type;
 
-enum						e_bool
+enum							e_bool
 {
 	FALSE = 0,
 	TRUE = 1
 };
 
-enum						e_quote_stt
+enum							e_quote_stt
 {
 	CLOSED = 0,
 	OPENED = 1
 };
 
-enum						e_error_stt
+enum							e_error_stt
 {
 	NOTHING = 0,
 	STX_QUOTE = 1,
@@ -39,5 +41,18 @@ enum						e_error_stt
 	HEREDOC_EOF = 5,
 	HEREDOC_SIGINT = 6,
 	HEREDOC_FD = 7
+};
+
+enum							e_redir_out_type
+{
+	RO_TRUNC = 1,
+	RO_APPEND = 2
+};
+
+enum							e_token_type
+{
+	WORD = 0,
+	REDIR_IN = 1,
+	REDIR_OUT = 2
 };
 #endif
--- includes/struct.h
+++ includes/struct.h
@@ -20,6 +20,12 @@ typedef struct s_env		t_env;
 typedef struct s_msh		t_msh;
 typedef struct s_quote_dt	t_quote_dt;
 typedef struct s_heredoc	t_heredoc;
+typedef struct s_arg		t_arg;
+typedef struct s_cmd		t_cmd;
+typedef struct s_redir_in	t_redir_in;
+typedef struct s_redir_out	t_redir_out;
+typedef struct s_prompt		t_prompt;
+
 struct						s_env
 {
 	char					*name;
@@ -46,6 +52,39 @@ struct						s_heredoc
 	char					*tmp_file;
 	t_bool					exp_var;
 	t_heredoc				*next;
+};
+
+struct						s_arg
+{
+	char					*value;
+	t_arg					*next;
+};
+
+struct						s_cmd
+{
+	char					*value;
+	t_arg					*arg_list;
+};
+
+struct						s_redir_in
+{
+	char					*file;
+	t_redir_in				*next;
+};
+
+struct						s_redir_out
+{
+	char					*file;
+	t_redir_out_type		*type;
+	t_redir_out				*next;
+}
+
+struct						s_prompt
+{
+	t_cmd					*cmd;
+	t_redir_in				redir_in_list;
+	t_redir_out				redir_out_list;
+	t_prompt				*next;
 };
 
 #endif
