99d47a86dbf9b4aa688e95f8c570a22eb66e21d0
crowley08 <saotraandrianom78@gmail.com>
2024-10-23 20:51:54 +0300

    echo rectified

--- srcs/builtin/ms_echo.c
+++ srcs/builtin/ms_echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ms_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arakotom <arakotom@student.42.fr>          +#+  +:+       +#+        */
+/*   By: saandria <saandria@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 13:56:49 by saandria          #+#    #+#             */
-/*   Updated: 2024/10/23 16:10:35 by arakotom         ###   ########.fr       */
+/*   Updated: 2024/10/23 20:51:33 by saandria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,22 @@ static void	print_with_n_option(t_arg *args)
 	}
 }
 
+static int check_option(t_arg *arg)
+{
+	int i;
+
+	i = 0;
+	if (arg->value[i] == '-')
+		i++;
+	while (arg->value[i])
+	{
+		if (arg->value[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 int	ms_echo(t_cmd *cmd)
 {
 	t_arg	*args;
@@ -40,12 +56,13 @@ int	ms_echo(t_cmd *cmd)
 		ft_putstr_fd("\n", STDOUT_FILENO);
 		return (0);
 	}
-	if (ft_strncmp(args->value, "-n", 2) == 0)
+	if (check_option(args) == 0)
 	{
-		args = args->next;
-		print_with_n_option(args);
-	}
-	else
 		print_with_no_option(args);
+		return (0);
+	}
+	while (args && check_option(args))
+		args = args->next;
+	print_with_n_option(args);
 	return (0);
 }
